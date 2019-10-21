bc58593446dbdfc3d8f7b7b1582baed3946abfaf
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-10-21 23:35:33 +0100

    Added the router.js file

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require("express");
 const app = express();
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 3000;
+
+
+// For the socketio server
+const http = require("http")
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}))
 
-// Routes
-app.get("/", (req, res) => {
-    res.status(200).json({
-        "message": "hello Internet"
-    })
-})
 
 
-
-app.listen(port, () => console.log(`server running on port ${port}`));
+server.listen(port, () => console.log(`server running on port ${port}`));
