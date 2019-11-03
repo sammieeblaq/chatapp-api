80f626d2fbdcdf2f8180aa35f1eb4d7690605cf0
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-11-03 09:35:30 +0100

    fixed bug in index.js

--- index.js
+++ index.js
@@ -13,8 +13,23 @@ io.on("connection", socket => {
     socket.on("join", ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room });
 
-        if (error) return callback(error);
-    })
+		if (error) return callback(error);
+
+		socket.emit("message", { user: "admin", text: `${user.room}, welcome to the room ${user.room}`});
+		socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name}, has joined`});
+
+		socket.join(user.room);
+
+		callback();
+	});
+	
+	// Waiting on sendMessage from the frontend to the backend
+	socket.on("sendMessage", (message, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit("message", { user: user.name, text: message });
+		callback();
+	});
 
     socket.on("disconnect", () => {
         console.log("User has disconnected");
