ff0c5099ec505b64fcc06803cb21185d8e3d12f1
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-11-03 16:33:04 +0100

    Completed the Backend

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ io.on("connection", socket => {
 
 		if (error) return callback(error);
 
-		socket.emit("message", { user: "admin", text: `${user.room}, welcome to the room ${user.room}`});
+		socket.emit("message", { user: "admin", text: `${user.name}, welcome to the room ${user.room}`});
 		socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name}, has joined`});
 
 		socket.join(user.room);
 
+		io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room)});
 		callback();
 	});
 	
@@ -28,11 +29,19 @@ io.on("connection", socket => {
 		const user = getUser(socket.id);
 
 		io.to(user.room).emit("message", { user: user.name, text: message });
+		io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room)});
+		
+		
 		callback();
 	});
 
     socket.on("disconnect", () => {
-        console.log("User has disconnected");
+		const user = removeUser(socket.id);
+		
+		// Message to notify that user left
+		if (user) {
+			io.to(user.room).emit("message", { user: "admin", text: `${user.name} has left.`})
+		}
     })
 });
 
